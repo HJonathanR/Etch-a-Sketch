8d7dd74ccf8221d0c0caff31630d2f9801404792
Jonathan <hramos30498@gmail.com>
2023-02-13 14:24:55 -0600

    added popup function to generate the grid size

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//Grid Size
-const btn = document.querySelector('btn');
-
-//Draw over and Generate Grid
 const body = document.querySelector('body');
 body.style.width = "900px";
 
@@ -9,23 +5,40 @@ const mainDiv = document.createElement('div');
 mainDiv.style.display = "flex"; 
 mainDiv.style.flexWrap = "wrap";
 
-function drawHover(e) {
-    console.log(e);
+//Grid Size
+function drawHover() {
     this.style.backgroundColor = "black";   
 }
 
-for (let x = 0; x <= 256; x++) {
-    const div = document.createElement('div');
-    div.classList.add("grid");
-    div.style.border = "solid";
-    div.style.margin = "0";
-    div.style.height = "50px";
-    div.style.width = "50px";
-
-    div.addEventListener('mouseover', drawHover);
-
-    mainDiv.appendChild(div);
+function popup() {
+    const sideSize = prompt("Enter the size of the etch-a-sketch grid:");
+    if(sideSize > 100) {
+        sideSize = 0;
+        alert("The size should be less than or equal to 100.");
+    } else {
+        return sideSize * sideSize;
+    }
+    
 }
+
+function generateGrid() {
+    const size = popup();
+    for (let x = 0; x <= size; x++) {
+        const div = document.createElement('div');
+        div.classList.add("grid");
+        div.style.border = "solid";
+        div.style.margin = "0";
+        div.style.height = "50px";
+        div.style.width = "50px";
+    
+        div.addEventListener('mouseover', drawHover);
+    
+        mainDiv.appendChild(div);
+    }
+}
+
+const btn = document.querySelector('#popup');
+btn.addEventListener('click', generateGrid);
 
 
 body.appendChild(mainDiv);
