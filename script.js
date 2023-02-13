c1f50da295cefe6fc888e52a48f6ca02b95a2877
Jonathan <hramos30498@gmail.com>
2023-02-13 13:42:55 -0600

    add function to draw over the grid and button

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-//Etch-A-Sketch
-const body = document.querySelector('body');
-const mainDiv = document.createElement('div')
+//Grid Size
+const btn = document.querySelector('btn');
 
-for (let x = 0; x <= 16; x++) {
+//Draw over and Generate Grid
+const body = document.querySelector('body');
+body.style.width = "900px";
+
+const mainDiv = document.createElement('div');
+mainDiv.style.display = "flex"; 
+mainDiv.style.flexWrap = "wrap";
+
+function drawHover(e) {
+    console.log(e);
+    this.style.backgroundColor = "black";   
+}
+
+for (let x = 0; x <= 256; x++) {
     const div = document.createElement('div');
     div.classList.add("grid");
     div.style.border = "solid";
+    div.style.margin = "0";
+    div.style.height = "50px";
+    div.style.width = "50px";
+
+    div.addEventListener('mouseover', drawHover);
 
     mainDiv.appendChild(div);
 }
+
 
 body.appendChild(mainDiv);
